80e628f1fca3416a4c0ca57c2e529eedc1fe6a95
summer <528150506@qq.com>
2023-01-30 20:44:25 +0800

    java

--- JavaBasic/src/com/basic/www/conpter07/Method01.java
+++ JavaBasic/src/com/basic/www/conpter07/Method01.java
@@ -16,7 +16,11 @@ public class Method01 {
         Person p1 = new Person();
         p1.speak();//调用方法
         p1.cal01();
-        p1.cal02(100);
+        p1.cal02(100);//调用cal02方法，同时给n = 100
+        int sum = p1.getSum(1, 2);
+        //调用getSum方法，同时a=1，b=2
+        //把方法getSum返回的值，赋给变量sum
+        System.out.println("getSum方法返回的值="+sum);
     }
 }
 
@@ -46,16 +50,25 @@ class Person {
     }
 
     //添加cal02成员方法，该方法可以接收一个数n，计算从1+...+n的结果
+    //(int n)形参列表 ，表示当前有一个形参n，可以接受用户输入
     public void cal02(int n) {
-            int sum=0;
-            for (int i = 1; i <= n; i++) {
-                sum+=n;
-            }
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += n;
+        }
         System.out.println("计算结果为：" + sum);
     }
 
     //添加getSum成员方法，可以计算两个数的和
-    public void getSum(int a, int b) {
-
+    /*
+     * 1、public表示方法是公开的
+     * 2、int：表示方法执行后，返回一个int值
+     * 3、getSum方法名
+     * 4、(int a, int b)形参列表，2个形参，可以接收用户传入的两个数
+     * 5、return sum;表示sum的值返回*/
+    public int getSum(int a, int b) {
+        int sum = a + b;
+        //System.out.println(a+"+"+b+"=" + sum);
+        return sum;
     }
 }
